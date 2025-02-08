f6050e23fc9e4a7d1e80d94df1034681da3d731e
Ryan Lee <12857462+ryanlee92@users.noreply.github.com>
2025-02-09 02:33:27 +0900

    Update script_builder.dart

--- lib/builders/script_builder.dart
+++ lib/builders/script_builder.dart
@@ -162,10 +162,8 @@ Name: "desktopicon"; Description: "{cm:CreateDesktopIcon}"; GroupDescription: "{
   String _icons() {
     return '''
 [Icons]
-AppUserModelID: "WaveCorporation.Taskey";
-AppUserModelToastActivatorCLSID: "6D809377-6AF0-444B-8957-A3773F02200E";
-Name: "{autoprograms}\\${config.name}"; Filename: "{app}\\${config.exeName}"
-Name: "{autodesktop}\\${config.name}"; Filename: "{app}\\${config.exeName}"; Tasks: desktopicon
+Name: "{autoprograms}\\${config.name}"; Filename: "{app}\\${config.exeName}"; AppUserModelID: "WaveCorporation.Taskey"; AppUserModelToastActivatorCLSID: "6D809377-6AF0-444B-8957-A3773F02200E";
+Name: "{autodesktop}\\${config.name}"; Filename: "{app}\\${config.exeName}"; Tasks: desktopicon; AppUserModelID: "WaveCorporation.Taskey"; AppUserModelToastActivatorCLSID: "6D809377-6AF0-444B-8957-A3773F02200E";
 \n''';
   }
 
