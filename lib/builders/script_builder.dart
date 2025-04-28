875fbf8916707672e14cbd061c3facb2096e038a
Ryan Lee <12857462+ryanlee92@users.noreply.github.com>
2025-04-28 21:40:03 +0900

    test2

--- lib/builders/script_builder.dart
+++ lib/builders/script_builder.dart
@@ -1,25 +1,4 @@
-/// This file contains the [ScriptBuilder] class, which is responsible for generating
-/// the Inno Setup Script (ISS) file for creating the installer of an application.
-///
-/// The [ScriptBuilder] class takes a [Config] object and an [appDir] directory as inputs
-/// and generates the ISS script based on the provided configuration. This script defines
-/// various sections like setup, files, icons, tasks, and languages, among others.
-///
-/// Key methods:
-/// - [_setup]: Generates the `[Setup]` section of the ISS script.
-/// - [_installDelete]: Generates the `[InstallDelete]` section.
-/// - [_languages]: Generates the `[Languages]` section.
-/// - [_tasks]: Generates the `[Tasks]` section.
-/// - [_files]: Generates the `[Files]` section.
-/// - [_icons]: Generates the `[Icons]` section.
-/// - [_run]: Generates the `[Run]` section.
-///
-/// The [build] method is the main method of this class, which combines all the sections and writes
-/// the complete ISS script to a file. It returns the generated script file.
-library;
-
 import 'dart:io';
-
 import 'package:inno_bundle/models/config.dart';
 import 'package:inno_bundle/models/admin_mode.dart';
 import 'package:inno_bundle/utils/cli_logger.dart';
@@ -27,36 +6,29 @@ import 'package:inno_bundle/utils/constants.dart';
 import 'package:inno_bundle/utils/functions.dart';
 import 'package:path/path.dart' as p;
 
-/// A class responsible for generating the Inno Setup Script (ISS) file for the installer.
 class ScriptBuilder {
-  /// The configuration guiding the script generation process.
   final Config config;
-
-  /// The directory containing the application files to be included in the installer.
   final Directory appDir;
 
-  /// Creates a [ScriptBuilder] instance with the given [config] and [appDir].
   ScriptBuilder(this.config, this.appDir);
 
-  /// Generates the `[Setup]` section of the ISS script, containing metadata and
-  /// configuration for the installer.
   String _setup() {
-  final outputDir = p.joinAll([
-    Directory.current.path,
-    ...installerBuildDir,
-    config.type.dirName,
-  ]);
-
-  var installerIcon = config.installerIcon;
-  if (installerIcon == defaultInstallerIconPlaceholder) {
-    final installerIconDirPath = p.joinAll([
-      Directory.systemTemp.absolute.path,
-      "${camelCase(config.name)}Installer",
+    final outputDir = p.joinAll([
+      Directory.current.path,
+      ...installerBuildDir,
+      config.type.dirName,
     ]);
-    installerIcon = persistDefaultInstallerIcon(installerIconDirPath);
-  }
 
-  return '''
+    var installerIcon = config.installerIcon;
+    if (installerIcon == defaultInstallerIconPlaceholder) {
+      final installerIconDirPath = p.joinAll([
+        Directory.systemTemp.absolute.path,
+        "${camelCase(config.name)}Installer",
+      ]);
+      installerIcon = persistDefaultInstallerIcon(installerIconDirPath);
+    }
+
+    return '''
 [Setup]
 AppId=${config.id}
 AppName=${config.name}
@@ -86,17 +58,82 @@ ShowLanguageDialog=no
 UseSetupLdr=no
 ${config.signTool != null ? config.signTool?.toInnoCode() : ""}
 \n''';
-}
+  }
 
-String _run() {
-  return '''
+  String _installDelete() {
+    return '''
+[InstallDelete]
+Type: filesandordirs; Name: "{app}\\*"
+\n''';
+  }
+
+  String _languages() {
+    return '''
+[Languages]
+Name: "english"; MessagesFile: "compiler:Default.isl"
+\n''';
+  }
+
+  String _tasks() {
+    return '''
+[Tasks]
+Name: "desktopicon"; Description: "{cm:CreateDesktopIcon}"; GroupDescription: "{cm:AdditionalIcons}";
+\n''';
+  }
+
+  String _files() {
+    var section = "[Files]\n";
+
+    final files = appDir.listSync();
+    for (final file in files) {
+      final filePath = file.absolute.path;
+      if (FileSystemEntity.isDirectorySync(filePath)) {
+        final fileName = p.basename(file.path);
+        section += "Source: \"$filePath\\*\"; DestDir: \"{app}\\$fileName\"; Flags: ignoreversion recursesubdirs createallsubdirs\n";
+      } else {
+        if (p.basename(filePath) == config.exePubspecName && config.exeName != config.exePubspecName) {
+          print("Renamed ${config.exePubspecName} ${config.exeName}");
+          section += "Source: \"$filePath\"; DestDir: \"{app}\"; DestName: \"${config.exeName}\"; Flags: ignoreversion\n";
+        } else {
+          section += "Source: \"$filePath\"; DestDir: \"{app}\"; Flags: ignoreversion\n";
+        }
+      }
+    }
+
+    final scriptDirPath = p.joinAll([
+      Directory.systemTemp.absolute.path,
+      "${camelCase(config.name)}Installer",
+      config.type.dirName,
+    ]);
+    Directory(scriptDirPath).createSync(recursive: true);
+    for (final fileName in vcDllFiles) {
+      final file = File(p.joinAll([...system32, fileName]));
+      if (!file.existsSync()) continue;
+      final fileNewPath = p.join(scriptDirPath, p.basename(file.path));
+      file.copySync(fileNewPath);
+      section += "Source: \"$fileNewPath\"; DestDir: \"{app}\";\n";
+    }
+
+    return '$section\n';
+  }
+
+  String _icons() {
+    return '''
+[Icons]
+Name: "{autoprograms}\\${config.name}"; Filename: "{app}\\${config.exeName}"; AppUserModelID: "WaveCorporation.Taskey"; AppUserModelToastActivatorCLSID: "6D809377-6AF0-444B-8957-A3773F02200E"; Flags: preventpinning;
+Name: "{autodesktop}\\${config.name}"; Filename: "{app}\\${config.exeName}"; Tasks: desktopicon; AppUserModelID: "WaveCorporation.Taskey"; AppUserModelToastActivatorCLSID: "6D809377-6AF0-444B-8957-A3773F02200E"; Flags: preventpinning;
+\n''';
+  }
+
+  String _run() {
+    return '''
 [Run]
 Filename: "{app}\\restart_helper.cmd"; Flags: nowait postinstall skipifsilent;
 \n''';
-}
+  }
 
-String _code() {
-  return '''
+  String _code() {
+    return '''
 [Code]
 procedure CurStepChanged(CurStep: TSetupStep);
 var
@@ -110,45 +147,45 @@ begin
       Log('❌ Failed to kill process')
     else
       Log('✅ Process killed, sleeping for 1 second...');
-    // Sleep 1000 ms to allow Windows to release file handles
     Sleep(1000);
   end;
 end;
 ''';
-}
+  }
 
-Future<File> build() async {
-  CliLogger.info("Generating ISS script...");
-  final script = scriptHeader +
-      _setup() +
-      _installDelete() +
-      _tasks() +
-      _files() +
-      _icons() +
-      _languages() +
-      _run() +
-      _code();
-  final relScriptPath = p.joinAll([
-    ...installerBuildDir,
-    config.type.dirName,
-    "inno-script.iss",
-  ]);
-  final absScriptPath = p.join(Directory.current.path, relScriptPath);
-  final scriptFile = File(absScriptPath);
-  scriptFile.createSync(recursive: true);
-  scriptFile.writeAsStringSync(script);
+  Future<File> build() async {
+    CliLogger.info("Generating ISS script...");
+    final script = scriptHeader +
+        _setup() +
+        _installDelete() +
+        _tasks() +
+        _files() +
+        _icons() +
+        _languages() +
+        _run() +
+        _code();
 
-  // 추가: restart_helper.cmd 생성
-  final restartHelperPath = p.join(appDir.path, "restart_helper.cmd");
-  final restartHelper = File(restartHelperPath);
-  restartHelper.writeAsStringSync('''
+    final relScriptPath = p.joinAll([
+      ...installerBuildDir,
+      config.type.dirName,
+      "inno-script.iss",
+    ]);
+    final absScriptPath = p.join(Directory.current.path, relScriptPath);
+    final scriptFile = File(absScriptPath);
+    scriptFile.createSync(recursive: true);
+    scriptFile.writeAsStringSync(script);
+
+    // 🧹 restart_helper.cmd 생성 (앱 종료 후 2초 대기, 재시작)
+    final restartHelperPath = p.join(appDir.path, "restart_helper.cmd");
+    final restartHelper = File(restartHelperPath);
+    restartHelper.writeAsStringSync('''
 @echo off
 timeout /t 2 > nul
 start "" "%~dp0${config.exeName}"
 exit
 ''');
 
-  CliLogger.success("Script generated $relScriptPath");
-  return scriptFile;
-}
+    CliLogger.success("Script generated $relScriptPath");
+    return scriptFile;
+  }
 }
