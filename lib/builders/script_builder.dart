df9a4eb52b6f67756c562c1d12bda3c1e8ac6039
Ryan Lee <12857462+ryanlee92@users.noreply.github.com>
2025-07-21 18:31:58 +0900

    savetest

--- lib/builders/script_builder.dart
+++ lib/builders/script_builder.dart
@@ -112,6 +112,24 @@ Root: "HKCU"; Subkey: "Software\\Microsoft\\Windows\\CurrentVersion\\Run"; Value
       config.type.dirName,
     ]);
     Directory(scriptDirPath).createSync(recursive: true);
+
+    // --- 이 부분을 수정합니다 ---
+    final vcRedistFiles = [
+      'VC_redist.x64.exe',
+      'VC_redist.x86.exe',
+      'VC_redist.arm64.exe',
+    ];
+    
+    for (final fileName in vcRedistFiles) {
+      final filePath = p.join(Directory.current.path, 'dependencies', fileName);
+      if (File(filePath).existsSync()) {
+        section += 'Source: "$filePath"; DestDir: "{tmp}"; Flags: deleteafterinstall\n';
+      } else {
+        CliLogger.warning('$fileName not found in "dependencies" folder. Skipping.');
+      }
+    }
+    // --- 여기까지 수정 ---
+    
     for (final fileName in vcDllFiles) {
       final file = File(p.joinAll([...system32, fileName]));
       if (!file.existsSync()) continue;
@@ -134,13 +152,58 @@ Name: "{autodesktop}\\${config.name}"; Filename: "{app}\\${config.exeName}"; Tas
   String _run() {
     return '''
 [Run]
+Filename: "{tmp}\\VC_redist.x64.exe"; Parameters: "/install /passive /norestart"; StatusMsg: "Installing Microsoft VC++ Runtime (x64)..."; Check: VCRedistNeedsInstall_x64
+Filename: "{tmp}\\VC_redist.x86.exe"; Parameters: "/install /passive /norestart"; StatusMsg: "Installing Microsoft VC++ Runtime (x86)..."; Check: VCRedistNeedsInstall_x86
+Filename: "{tmp}\\VC_redist.arm64.exe"; Parameters: "/install /passive /norestart"; StatusMsg: "Installing Microsoft VC++ Runtime (arm64)..."; Check: VCRedistNeedsInstall_arm64
 Filename: "{app}\\${config.exeName}"; Description: "{cm:LaunchProgram,{#StringChange('${config.name}', '&', '&&')}}"; Flags: nowait postinstall skipifsilent shellexec runascurrentuser
 \n''';
   }
 
   String _code() {
-    return '''
+  return '''
 [Code]
+// 공통 헬퍼 함수: 특정 아키텍처의 VC++ 런타임이 설치되었는지 확인
+function IsVCRedistInstalled(const Arch: String): Boolean;
+var
+  Version: String;
+  Key: String;
+begin
+  if Arch = 'x64' then
+    Key := 'SOFTWARE\\Microsoft\\VisualStudio\\14.0\\VC\\Runtimes\\x64'
+  else if Arch = 'x86' then
+    Key := 'SOFTWARE\\WOW6432Node\\Microsoft\\VisualStudio\\14.0\\VC\\Runtimes\\x86'
+  else if Arch = 'arm64' then
+    Key := 'SOFTWARE\\Microsoft\\VisualStudio\\14.0\\VC\\Runtimes\\arm64'
+  else begin
+    Result := True; // 알 수 없는 아키텍처는 이미 설치된 것으로 간주
+    exit;
+  end;
+
+  if RegQueryStringValue(HKLM, Key, 'Version', Version) then
+    Result := True
+  else
+    Result := False;
+end;
+
+// x64용 체크 함수
+function VCRedistNeedsInstall_x64: Boolean;
+begin
+  Result := (GetProcessorArchitecture = 'x64') and (not IsVCRedistInstalled('x64'));
+end;
+
+// x86용 체크 함수
+function VCRedistNeedsInstall_x86: Boolean;
+begin
+  // x64 윈도우에서는 x86 앱도 실행 가능하므로, 두 경우 모두 체크
+  Result := ((GetProcessorArchitecture = 'x86') or (GetProcessorArchitecture = 'x64')) and (not IsVCRedistInstalled('x86'));
+end;
+
+// arm64용 체크 함수
+function VCRedistNeedsInstall_arm64: Boolean;
+begin
+  Result := (GetProcessorArchitecture = 'arm64') and (not IsVCRedistInstalled('arm64'));
+end;
+
 procedure CurStepChanged(CurStep: TSetupStep);
 var
   ResultCode: Integer;
@@ -157,7 +220,7 @@ begin
   end;
 end;
 ''';
-  }
+}
 
   Future<File> build() async {
     CliLogger.info("Generating ISS script...");
