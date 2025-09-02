a5083acafc7a54ec49b1a5f3807524af6a46277b
Ryan Lee <12857462+ryanlee92@users.noreply.github.com>
2025-09-03 01:53:01 +0900

    Update script_builder.dart

--- lib/builders/script_builder.dart
+++ lib/builders/script_builder.dart
@@ -54,6 +54,7 @@ ArchitecturesAllowed=${config.arch.value}
 ArchitecturesInstallIn64BitMode=${config.arch.value}
 DisableDirPage=auto
 DisableProgramGroupPage=auto
+ChangesAssociations=yes
 ShowLanguageDialog=no
 CloseApplications=no
 ${config.signTool != null ? config.signTool?.toInnoCode() : ""}
